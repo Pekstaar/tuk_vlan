72945981916ec46d4c6a5c7a9ffc2093e28ede18
Pekstaar <pekstaar@gmail.com>
2022-09-29 17:14:29 +0300

    [UPDATE]: record screen

--- src/components/MeetingFooter/MeetingFooter.component.js
+++ src/components/MeetingFooter/MeetingFooter.component.js
@@ -11,12 +11,17 @@ import {
 import ReactTooltip from "react-tooltip";
 import "./MeetingFooter.css";
 import { FaLink } from "react-icons/fa";
+import { IoRecordingSharp } from "react-icons/io5";
+import { BsRecordCircle } from "react-icons/bs";
+import { connect } from "react-redux";
 const MeetingFooter = (props) => {
   const [streamState, setStreamState] = useState({
     mic: true,
     video: false,
     screen: false,
   });
+  const [isRecording, setIsRecording] = useState(false);
+
   const micClick = () => {
     setStreamState((currentState) => {
       return {
@@ -51,6 +56,49 @@ const MeetingFooter = (props) => {
       };
     });
   };
+
+  const chunks = [];
+  var recorder;
+
+  const record = (stream) => {
+    recorder = new MediaRecorder(stream, {
+      mineType: "video/webm;codecs=H264",
+    });
+    recorder.onstop = (e) => {
+      recorder = null;
+    };
+    recorder.ondataavailable = (e) => {
+      chunks.push(e.data);
+    };
+  };
+
+  const handleRecording = () => {
+    // recording start
+    setIsRecording(!isRecording);
+
+    if (isRecording === false) {
+      // recording
+      record(props?.stream);
+      recorder?.start(1000);
+    }
+    // recording stop
+    else {
+      const completeBlob = new Blob(chunks, { type: chunks[0]?.type });
+      var anchor = document.createElement("a");
+      document.body.appendChild(anchor);
+      anchor.style = "display: none";
+      var url = window.URL.createObjectURL(completeBlob);
+      anchor.href = url;
+      anchor.download = `aaaa.mp4`;
+      anchor.click();
+      window.URL.revokeObjectURL(url);
+      recorder?.stop();
+
+      while (chunks.length) {
+        chunks.pop();
+      }
+    }
+  };
   useEffect(() => {
     props.onMicClick(streamState.mic);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -61,6 +109,11 @@ const MeetingFooter = (props) => {
   }, [streamState.video]);
   return (
     <div className="meeting-footer">
+      {isRecording && (
+        <div className={"absolute top-4 left-4 z-[200] text-white"}>
+          <BsRecordCircle className={"text-4xl text-red-500 "} />
+        </div>
+      )}
       <div
         className={"meeting-icons " + (!streamState.mic ? "active" : "")}
         data-tip={streamState.mic ? "Mute Audio" : "Unmute Audio"}
@@ -98,6 +151,15 @@ const MeetingFooter = (props) => {
 
       <div
         className="meeting-icons"
+        data-tip={isRecording ? "Start recording Meeting" : "Stop recording"}
+        onClick={handleRecording}
+        // disabled={streamState.screen}
+      >
+        <IoRecordingSharp />
+      </div>
+
+      <div
+        className="meeting-icons"
         data-tip="Copy Link"
         onClick={props.onToggleLinkCard}
         // disabled={streamState.screen}
@@ -109,4 +171,9 @@ const MeetingFooter = (props) => {
   );
 };
 
-export default MeetingFooter;
+const mapStateToProps = (state) => {
+  return {
+    stream: state.mainStream,
+  };
+};
+export default connect(mapStateToProps)(MeetingFooter);
