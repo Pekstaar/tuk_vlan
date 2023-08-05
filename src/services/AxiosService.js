8d5c965e60235324fb316b3ed6db9172eb4487c3
Pekstaar <74072378+Pekstaar@users.noreply.github.com>
2023-08-05 19:01:46 +0300

    Update AxiosService.js backend url

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import localStorageService from "./LocalStorageService";
 
 // export const ENDPOINT = "http://localhost:5500";
-export const ENDPOINT = "https://tuk-vlan.onrender.com";
+export const ENDPOINT = "https://tuk-vlan-2.onrender.com";
 const BASE_URL = ENDPOINT + "/api";
 
 const AxiosUtility = axios.create({
