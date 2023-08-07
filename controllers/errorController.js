68087f0354092a9538d73fd0ef418ff60257fcec
Pekstaar <74072378+Pekstaar@users.noreply.github.com>
2023-08-07 11:05:09 +0300

    fix: UnhandledPromiseRejectionWarning: ReferenceError: Cannot access 'Error' before initialization

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,10 +2,10 @@ const Error = require('../models/ErrorModel');
 const { getErrorMessage } = require("../middleware/errorMiddleWare");
 
 const create=async(req,res,next)=>{
-    let Error = new Error({error:req.body})
+    let CError = new Error({error:req.body})
 
     try{
-        let error = await Error.save();
+        let error = await CError.save();
 
         res.json(error)
     } catch (err){
